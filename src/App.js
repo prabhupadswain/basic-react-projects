383a1e3e71e3b6940424733299f448e83227c0d1
prabhupadswain <prabhupad.swain@outlook.com>
2022-01-22 11:44:13 +0530

    App component changed from function to class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className='App'>
-      <Navbar />
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
