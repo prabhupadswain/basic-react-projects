a343b31b2b9ab5f8b1c29828c63fe054f7c2cabc
prabhupadswain <prabhupad.swain@outlook.com>
2022-01-22 11:29:27 +0530

    Navbar & Header components added

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <h1>Welcome to our React Community!</h1>
+        <p>
+          Here, you can find a lot of resources related to React. You can
+          interact with lot of other techies who who are here to learn React.
+          Help, yourself and each other. Expand your learning.
+        </p>
+      </>
+    );
+  }
+}
+
+export default Header;
