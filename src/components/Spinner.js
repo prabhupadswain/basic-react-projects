f737fc355afc7932e3637742637c7c4b6d12bcdb
prabhupadswain <prabhupad.swain@outlook.com>
2022-02-03 09:01:29 +0530

    Spinner Component modified to change className from class

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 class Spinner extends Component {
   render() {
     return (
-      <div class='container mt-3'>
+      <div className='container mt-3'>
         <h2>Colored Spinners</h2>
        
-        <div class='spinner-border text-muted'></div>
-        <div class='spinner-border text-primary'></div>
-        <div class='spinner-border text-success'></div>
-        <div class='spinner-border text-info'></div>
-        <div class='spinner-border text-warning'></div>
-        <div class='spinner-border text-danger'></div>
-        <div class='spinner-border text-secondary'></div>
-        <div class='spinner-border text-dark'></div>
-        <div class='spinner-border text-light'></div>
+        <div className='spinner-border text-muted'></div>
+        <div className='spinner-border text-primary'></div>
+        <div className='spinner-border text-success'></div>
+        <div className='spinner-border text-info'></div>
+        <div className='spinner-border text-warning'></div>
+        <div className='spinner-border text-danger'></div>
+        <div className='spinner-border text-secondary'></div>
+        <div className='spinner-border text-dark'></div>
+        <div className='spinner-border text-light'></div>
       </div>
     );
   }
